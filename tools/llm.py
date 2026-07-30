2519f78be8cc1ffdb30adfc1f53f7b96ada224bc
Mythili-Pandiyarajan <mythilipandi18@gmail.com>
2026-07-30 20:54:11 +0530

    Update llm.py

--- tools/llm.py
+++ tools/llm.py
@@ -12,14 +12,27 @@ client = Groq(
 
 def groq_call(prompt):
 
+    # Debug
+    print("PROMPT TYPE:", type(prompt))
+    print("PROMPT:", prompt)
+
+    if not isinstance(prompt, str):
+        prompt = str(prompt)
+
     response = client.chat.completions.create(
         model="llama-3.1-8b-instant",
         messages=[
             {
+                "role": "system",
+                "content": "You are an expert SOC analyst."
+            },
+            {
                 "role": "user",
                 "content": prompt
             }
-        ]
+        ],
+        temperature=0.2,
+        max_tokens=1000
     )
 
     return response.choices[0].message.content
